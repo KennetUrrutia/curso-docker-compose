901ebcc585da0e4bef63fb429e675a2e707408b1
Kenneth De Leon <Kenneth@somosmangoverde.com>
2023-02-13 18:05:11 -0600

    docker compose crear contenedor de mongo conautentificacion

--- Curso-Udemy/nest-graphql/src/todo/todo.service.ts
+++ Curso-Udemy/nest-graphql/src/todo/todo.service.ts
@@ -12,6 +12,7 @@ export class TodoService {
         { id: 3, description: 'Piedra del Poder', done: false },
         { id: 4, description: 'Piedra del Tiempo', done: false },
         { id: 5, description: 'Piedra de la Realidad', done: true },
+	    { id: 6, description: 'Piedra del conocimiento', done: true },
     ];
 
     get totalTodos() {
